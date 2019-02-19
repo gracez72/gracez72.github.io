9038b0d476be861355db7106761a5cd48880b29f
gracez72 <gracezhang72@gmail.com>
2019-02-18 19:58:40 -0800

    Updates

---
+++ precache-manifest.efbf45964aa377afe93b4dfa9ea72b92.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/gracez72.github.io/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "a3859b706eede8f64600",
+    "url": "/gracez72.github.io/static/css/main.f15ed5d7.chunk.css"
+  },
+  {
+    "revision": "d2feda21dc33b28b86fc",
+    "url": "/gracez72.github.io/static/js/runtime~main.d2feda21.js"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/gracez72.github.io/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "dc2183bea592661b8874",
+    "url": "/gracez72.github.io/static/js/2.dc2183be.chunk.js"
+  },
+  {
+    "revision": "7e31e03460bab3c5519eda2d77f2ea77",
+    "url": "/gracez72.github.io/static/media/tent.7e31e034.jpg"
+  },
+  {
+    "revision": "afcf6e0a364336de11c031993833f606",
+    "url": "/gracez72.github.io/static/media/pi.afcf6e0a.jpg"
+  },
+  {
+    "revision": "3ba6029ffa40e05f82d5572c8a87d3c9",
+    "url": "/gracez72.github.io/static/media/blue_green_vector.3ba6029f.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/gracez72.github.io/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/gracez72.github.io/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/gracez72.github.io/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/gracez72.github.io/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "a3859b706eede8f64600",
+    "url": "/gracez72.github.io/static/js/main.a3859b70.chunk.js"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/gracez72.github.io/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/gracez72.github.io/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/gracez72.github.io/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/gracez72.github.io/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/gracez72.github.io/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/gracez72.github.io/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/gracez72.github.io/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/gracez72.github.io/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/gracez72.github.io/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/gracez72.github.io/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "dc2183bea592661b8874",
+    "url": "/gracez72.github.io/static/css/2.ef8e5e59.chunk.css"
+  },
+  {
+    "revision": "20a71d69d56a9f310426bd45ba875eb4",
+    "url": "/gracez72.github.io/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/gracez72.github.io/precache-manifest.efbf45964aa377afe93b4dfa9ea72b92.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/gracez72.github.io/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
